2f226f14b5e857753a719f68347743d000e1aace
PrashanthKumarMadhasu <madhasuprashanth15@gmail.com>
2024-05-02 17:25:05 +0530

    PC-3 login added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
-import './App.css';
+import { LoginForm } from './Components/LoginForm/LoginForm';
 
 function App() {
   return (
-    <>
-    </>
+    <div>
+      <LoginForm/>
+    </div>
   );
 }
 
